433494418143f49a06070e544018f54e8fe46c9f
bifo90 <stefanobifolco@gmail.com>
2024-05-02 15:31:05 +0200

    shadcn e livereload

--- Gemfile
+++ Gemfile
@@ -26,9 +26,8 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
-
 # Use Redis adapter to run Action Cable in production
-# gem "redis", ">= 4.0.1"
+gem "redis", ">= 4.0.1"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
@@ -53,6 +52,8 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem "rails_live_reload"
+
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import preset from "franken-ui/shadcn-ui/preset";
+import variables from "franken-ui/shadcn-ui/variables";
+import ui from "franken-ui";
+import hooks from "franken-ui/shadcn-ui/hooks";
 
-module.exports = {
+const shadcn = hooks({ theme: 'green' });
+
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+  presets: [preset],
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    variables({ theme: 'green' }),
+    ui({
+      components: {
+        alert: {
+          hooks: shadcn.alert
+        },
+        card: {
+          hooks: shadcn.card
+        },
+        'form-range': {
+          hooks: shadcn["form-range"]
+        },
+        form: {
+          hooks: shadcn.form,
+          media: true
+        },
+        button: {
+          hooks: shadcn.button
+        }
+      }
+    }),
+  ],
+};
